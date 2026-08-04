67ef3ca922c23d346159eb667157b7797fc64b30
eneone1 <jamesidakwoji@gmail.com>
2026-08-04 10:00:14 +0100

    feat: define IUserRepository, IAuthService and ITokenService interfaces

---
+++ AuthService.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using AuthService.Domain.Entities;
+using System.Security.Claims;
+
+namespace AuthService.Application.Interfaces
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(User user);
+        string GenerateRefreshToken();
+        ClaimsPrincipal? GetPrincipalFromExpiredToken(string token);
+    }
+}
